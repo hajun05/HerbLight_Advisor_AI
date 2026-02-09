3b22a6e67b4d2e111ca5132b0db784d66c7f2e5f
hajun05 <hajun0325175@gmail.com>
2026-02-09 14:42:54 +0900

    fix: 파이썬 AI 폴더와 C# WPF 폴더 분리, WPF 프로젝트 생성

---
+++ WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HerbLight_Advisor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
